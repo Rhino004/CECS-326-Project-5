85b3901c1b3cd22a036b2ec07aca99088011fe77
Ryan Tomas <113748661+Rhino004@users.noreply.github.com>
2024-12-05 19:27:57 -0800

    filling out the FIFO and LRU algorithm still need to be tested

--- paging.c
+++ paging.c
@@ -30,11 +30,87 @@ void PrintMemoryState(int memory[], int frameSize){
 // FIFO Page Replacement Algorithm
 void FIFO(int pages[], int pageCount, int frameSize)
 {
+    printf("\nFIFO Algorithm:\n");
+    int memory[MAX_FRAMES];
+    int front = 0; // Points to the oldest page
+    int pageFaults = 0;
+
+    // Initialize memory with -1
+    for (int i = 0; i < frameSize; i++)
+    {
+        memory[i] = -1;
+    }
+
+    for (int i = 0; i < pageCount; i++)
+    {
+        if (!isPageInMemory(pages[i], memory, frameSize))
+        {
+            memory[front] = pages[i];
+            front = (front + 1) % frameSize; // Circular queue logic
+            pageFaults++;
+            printf("Page %d caused a fault. ", pages[i]);
+        }
+        else
+        {
+            printf("Page %d did not cause a fault. ", pages[i]);
+        }
+        printMemoryState(memory, frameSize);
+    }
+
+    printf("Total Page Faults (FIFO): %d\n", pageFaults);
 }
 
 // LRU Page Replacement Algorithm
 void LRU(int pages[], int pageCount, int frameSize){
+    printf("\nLRU Algorithm:\n");
+    int memory[MAX_FRAMES], recent[MAX_FRAMES];
+    int pageFaults = 0;
 
+    // Initialize memory and recent usage trackers
+    for (int i = 0; i < frameSize; i++)
+    {
+        memory[i] = -1;
+        recent[i] = -1;
+    }
+
+    for (int i = 0; i < pageCount; i++)
+    {
+        if (!isPageInMemory(pages[i], memory, frameSize))
+        {
+            int lruIndex = 0;
+
+            // Find the least recently used page
+            for (int j = 1; j < frameSize; j++)
+            {
+                if (recent[j] < recent[lruIndex])
+                {
+                    lruIndex = j;
+                }
+            }
+
+            memory[lruIndex] = pages[i];
+            pageFaults++;
+            printf("Page %d caused a fault. ", pages[i]);
+        }
+        else
+        {
+            printf("Page %d did not cause a fault. ", pages[i]);
+        }
+
+        // Update recent usage
+        for (int j = 0; j < frameSize; j++)
+        {
+            if (memory[j] != -1)
+            {
+                recent[j]++;
+            }
+        }
+        recent[i % frameSize] = 0;
+
+        printMemoryState(memory, frameSize);
+    }
+
+    printf("Total Page Faults (LRU): %d\n", pageFaults);
 }
 
 int main() {
